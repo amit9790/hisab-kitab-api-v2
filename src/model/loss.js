a3b9f46a7c50ef52d2eb93977abe83ee178939a2
Vashesh08 <vashesh2001@gmail.com>
2024-04-30 19:26:39 +0530

    Changes for Loss Bk

---
+++ src/model/loss.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+module.exports = function (fastify, opts, next) {
+  const Schema = mongoose.Schema;
+  const { v4: uuidv4 } = require('uuid');
+
+  const lossSchema = new mongoose.Schema(
+    {
+      _id: {type: String, default:  () => { let res = uuidv4(); return res.id}, alias: "loss_id" },
+
+      date: {
+        type: Date,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+        enums: ["Melting", "Kareegar"],
+      },
+      transactionId: {
+        type: String,
+        required: true,
+      },
+      lossWt: {
+        type: String,
+        required: true,
+      },
+      createdBy: {
+        type: String,
+        required: false,
+      },
+      modifiedBy: {
+        type: String,
+        required: false,
+      },
+      is_deleted_flag: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+      user_id: { type: Schema.Types.ObjectId, ref: "User" },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+  mongoose.model("loss", lossSchema);
+  next();
+};
